c32b68bf147e9a922a73fb778f3de13ffe259058
random-jordan <jam35d12@gmail.com>
2022-01-06 15:50:22 +0500

    styles changes

---
+++ views/itpark/views/about.js
@@ -0,0 +1,39 @@
+new Splide('.splide-acts', {
+    perPage: 4,
+    perMove: 1,
+    padding: '30px',
+    pagination: false,
+    breakpoints: {
+        1400: {
+            perPage: 3,
+            perMove: 1,
+        },
+        992: {
+            perPage: 2,
+            perMove: 1,
+        },
+        768: {
+            perPage: 1,
+            perMove: 1,
+        },
+    }
+}).mount()
+
+new Splide('.splide-members', {
+    type: 'loop',
+    perPage: 4,
+    perMove: 1,
+    lazyLoad: 'nearby',
+    padding: '50px',
+    pagination: false,
+    breakpoints: {
+        992: {
+            perPage: 3,
+            perMove: 1,
+        },
+        540: {
+            perPage: 2,
+            perMove: 1,
+        }
+    }
+}).mount()
